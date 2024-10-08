103949af91d8a1633a70fb728a438f8e1104fa98
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-10-08 13:07:31 +0800

    release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import android.databinding.tool.ext.capitalizeUS
+import com.android.build.gradle.tasks.PackageAndroidArtifact
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -17,8 +20,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            signingConfig = null
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -32,6 +38,22 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = true
+    }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+    packaging {
+        resources {
+            excludes += "**"
+        }
+    }
+    tasks.withType<PackageAndroidArtifact> {
+        doFirst { appMetadata.asFile.orNull?.writeText("") }
+    }
 }
 
 dependencies {
@@ -42,26 +64,36 @@ dependencies {
 }
 
 afterEvaluate {
-    val apkPath = layout.buildDirectory.file("outputs/apk/debug/app-debug.apk").get().asFile.absolutePath
-    val shPath = layout.projectDirectory.file("scripts/ars.sh").asFile.absolutePath
-    task<Task>("pushByADB") {
-        group = "push"
-        dependsOn("assembleDebug")
-        doLast {
-            exec {
-                commandLine("adb", "shell", "rm /data/local/tmp/ars.apk /data/local/tmp/ars || su -c 'rm /data/local/tmp/ars.apk /data/local/tmp/ars'")
-                isIgnoreExitValue = true
+    android.applicationVariants.forEach { variant ->
+        val variantLowered = variant.name.lowercase()
+        val variantCapped = variant.name.capitalizeUS()
+        val apkPath =
+            layout.buildDirectory.file("outputs/apk/$variantLowered/app-$variantLowered-unsigned.apk")
+                .get().asFile.absolutePath
+        val shPath = layout.projectDirectory.file("scripts/ars.sh").asFile.absolutePath
+        task<Task>("pushByADB$variantCapped") {
+            group = "push"
+            dependsOn("assemble$variantCapped")
+            doLast {
+                exec {
+                    commandLine(
+                        "adb",
+                        "shell",
+                        "rm /data/local/tmp/ars.apk /data/local/tmp/ars || su -c 'rm /data/local/tmp/ars.apk /data/local/tmp/ars'"
+                    )
+                    isIgnoreExitValue = true
+                }
+                exec {
+                    commandLine("adb", "push", apkPath, "/data/local/tmp/ars.apk")
+                }
+                exec {
+                    commandLine("adb", "push", shPath, "/data/local/tmp/ars")
+                }
+                exec {
+                    commandLine("adb", "shell", "chmod +x /data/local/tmp/ars")
+                }
             }
-            exec {
-                commandLine("adb", "push", apkPath, "/data/local/tmp/ars.apk")
-            }
-            exec {
-                commandLine("adb", "push", shPath, "/data/local/tmp/ars")
-            }
-            exec {
-                commandLine("adb", "shell", "chmod +x /data/local/tmp/ars")
-            }
-        }
 
+        }
     }
 }
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,21 +1,13 @@
-# Add project specific ProGuard rules here.
-# You can control the set of applied configuration files using the
-# proguardFiles setting in build.gradle.
-#
-# For more details, see
-#   http://developer.android.com/guide/developing/tools/proguard.html
+-repackageclasses
+-allowaccessmodification
+-overloadaggressively
+-renamesourcefileattribute
 
-# If your project uses WebView with JS, uncomment the following
-# and specify the fully qualified class name to the JavaScript interface
-# class:
-#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-#   public *;
-#}
+-keepclasseswithmembers class io.github.a13e300.appresolveserver.MainKt {
+    public static void main(java.lang.String[]);
+}
 
-# Uncomment this to preserve the line number information for
-# debugging stack traces.
-#-keepattributes SourceFile,LineNumberTable
-
-# If you keep the line number information, uncomment this to
-# hide the original source file name.
-#-renamesourcefileattribute SourceFile
+-keep class io.github.a13e300.appresolveserver.PackageItem { *; }
+-keep class io.github.a13e300.appresolveserver.UserItem { *; }
+-keep class io.github.a13e300.appresolveserver.Request { *; }
+-keep class io.github.a13e300.appresolveserver.Response { *; }
